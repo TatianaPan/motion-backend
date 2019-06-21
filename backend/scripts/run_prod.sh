2691e2f371998e99e20cca78f8f364a67347af65
Tatiana Panferova <tatiana.panferova@gmail.com>
2019-06-21 14:14:14 +0200

    fixed path in run_dev.sh and run_prod.sh

--- backend/scripts/run_prod.sh
+++ backend/scripts/run_prod.sh
@@ -1,3 +1,3 @@
 python manage.py migrate
 python manage.py collectstatic --no-input
-/opt/conda/envs/django-app/bin/gunicorn -w 4 -b 0.0.0.0:8000 app.wsgi:application
+/opt/conda/envs/django_motion/bin/gunicorn -w 4 -b 0.0.0.0:8000 app.wsgi:application
